fa2e8610e8bc164e45ddac1010b8278a1b34f9b3
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2020-04-16 10:41:51 -0400

    Replase Java signer with Go signer

--- android/app/src/main/kotlin/com/example/arweave/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/arweave/MainActivity.kt
@@ -10,32 +10,25 @@ import java.security.KeyFactory
 import java.security.spec.RSAPrivateKeySpec
 import java.math.BigInteger
 import java.util.Base64
+import signer.Signer
+import android.util.Log
+import org.json.JSONObject
 
 class MainActivity: FlutterActivity() {
     private val CHANNEL = "armob.dev/signer"
-
-    fun signTransaction(rawTransaction: ByteArray? , n : String?, d : String?): ByteArray? {
-        val fact = KeyFactory.getInstance("RSA")
-        val spec = RSAPrivateKeySpec(BigInteger(n),BigInteger(d))
-        val priv = fact.generatePrivate(spec)
-        val s = Signature.getInstance("SHA256withRSA/PSS")
-                .apply {
-                    initSign(priv)
-                    update(rawTransaction)
-                }
-        return s.sign()
-    }
 
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
             call, result ->
-            if (call.method == "signTransaction") {
+            if (call.method == "sign") {
                 val rawTransaction = call.argument<ByteArray>("rawTransaction")
                 val n = call.argument<String>("n")
                 val d = call.argument<String>("d")
-                val signature = signTransaction(rawTransaction, n, d)
-                result.success(signature)
+                val dp = call.argument<String>("dp")
+                val dq = call.argument<String>("dq")
+                Log.d("D",d)
+                result.success(Signer.sign(rawTransaction, n, d, dp, dq))
             }
             else {
                 result.notImplemented()
