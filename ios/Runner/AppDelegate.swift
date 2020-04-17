a4beaa6aca426558bb2e83f21bb495680d5f5dce
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2020-04-17 14:11:04 -0400

    Add support for iOS (#43)
    iOS support

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import Signer
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,30 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let signerChannel = FlutterMethodChannel(name: "armob.dev/signer",
+                                              binaryMessenger: controller.binaryMessenger)
+    signerChannel.setMethodCallHandler({
+      (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+        if (call.method == "sign") {
+            let args = call.arguments as? Dictionary<String, Any>
+            let rawTx = args?["rawTransaction"] as? FlutterStandardTypedData
+            let n = args?["n"] as? String
+            let d = args?["d"] as? String
+            let dp = args?["dp"] as? String
+            let dq = args?["dq"] as? String
+            var error: NSErrorPointer = nil
+            var rawTxData = Data(rawTx!.data)
+            
+            result(SignerSign(rawTxData, n, d, dp, dq, error))
+        }
+        else {
+           result(FlutterMethodNotImplemented)
+           return
+         }
+        
+    })
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
