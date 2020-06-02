1785298de9c953a68863a42112fd65393c32ec6d
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2020-06-02 06:29:36 -0400

    Hackified walletAPI

--- lib/browser.dart
+++ lib/browser.dart
@@ -1,9 +1,14 @@
 import 'dart:convert';
 import 'package:arweave/ens.dart';
+import 'package:jose/jose.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:provider/provider.dart';
 import 'package:arweave/appState.dart';
+import 'package:libarweave/libarweave.dart' as Arweave;
+import 'package:flutter/services.dart';
+import 'dart:typed_data';
+import 'package:pointycastle/export.dart';
 
 var loginFunction, signFunction;
 
@@ -12,16 +17,27 @@ final webviewKey = GlobalKey<WebViewContainerState>();
 final oldSigningFunction =
     'oldSigningFunction = arweave.transactions.sign; alert("ArMob will manage message signing in this Dapp");';
 final signingFunction = '''arweave.transactions.sign = async function() {
-if (confirm(`Transaction Fee \${arweave.ar.winstonToAr(arguments[0].reward)} AR. Do you want to sign this transaction?`)) {
-result = await oldSigningFunction.apply(this, arguments);
-return result;
-}
-else { alert('Transaction canceled')}}''';
+   window.flutter_inappwebview.callHandler('sign',arguments).then(function(result) {
+     if (confirm(`Transaction Fee \${arweave.ar.winstonToAr(arguments[0].reward)} AR. Do you want to sign this transaction?`)) {
+    arguments[0].signature = result[0];
+    arguments[0].id = result[1];
+    console.log(arguments[0]);
+    return arguments[0];  }
+else { alert('Transaction canceled')}});
+}''';
 
 final unlockFunction =
     '''queries = (Array.from(document.getElementsByTagName('script'))).filter(script => script.text.includes("new FileReader"));
           re = /(?<=function\\s+)(\\w+)(?=\\s*\\(\\w*\\)\\s*\\{[\\s\\S]+new FileReader[\\s\\S]*})/;
           loginFunctionName = (queries[0].text.match(re))[0];''';
+
+BigInt _base64ToInt(String encoded) {
+  final b256 = new BigInt.from(256);
+  encoded += new List.filled((4 - encoded.length % 4) % 4, "=").join();
+  return base64Url
+      .decode(encoded)
+      .fold(BigInt.zero, (a, b) => a * b256 + new BigInt.from(b));
+}
 
 class WebViewContainer extends StatefulWidget {
   WebViewContainer({Key key}) : super(key: key);
@@ -33,6 +49,8 @@ class WebViewContainer extends StatefulWidget {
 class WebViewContainerState extends State<WebViewContainer> {
   InAppWebViewController webViewController;
   double _progress = 0;
+  static const platform = const MethodChannel('armob.dev/signer');
+
   @override
   Widget build(BuildContext context) {
     return Column(children: <Widget>[
@@ -44,7 +62,7 @@ class WebViewContainerState extends State<WebViewContainer> {
       Expanded(
           child: InAppWebView(
               initialUrl:
-                  "https://ftesrg4ur46h.arweave.net/nej78d0EJaSHwhxv0HAZkTGk0Dmc15sChUYfAC48QHI/index.html",
+                  "https://alz4bdsrvmoz.arweave.net/fGUdNmXFmflBMGI2f9vD7KzsrAc1s1USQgQLgAVT0W0",
               initialHeaders: {},
               initialOptions: InAppWebViewWidgetOptions(
                   crossPlatform: InAppWebViewOptions(debuggingEnabled: true)),
@@ -57,8 +75,48 @@ class WebViewContainerState extends State<WebViewContainer> {
                       loginFunction = args[0];
                       signFunction = args[1];
                       debugPrint('ArMob found a signing function!');
-                      debugPrint(args.toString());
+                      debugPrint("Arguments are ${args.toString()}");
                       return "Still seeing you";
+                    });
+                controller.addJavaScriptHandler(
+                    handlerName: "sign",
+                    callback: (args) async {
+                      final keyString =
+                          Provider.of<WalletData>(context, listen: false)
+                              .walletString;
+                      Arweave.Wallet _myWallet =
+                          Arweave.Wallet(jsonWebKey: keyString);
+                      debugPrint('Hi ${args[0]['0'].toString()}',
+                          wrapWidth: 1000);
+                      debugPrint('Wallet key: ${_myWallet.address}');
+                      List<int> transaction = _myWallet.createTransaction(
+                          args[0]['0']['last_tx'], args[0]['0']['reward'],
+                          data: Arweave.decodeBase64EncodedBytes(
+                              args[0]['0']['data']),
+                          tags: Arweave.decodeTags(args[0]['0']['tags']),
+                          targetAddress: args[0]['0']['target'],
+                          quantity: args[0]['0']['quantity']);
+
+                      final signedTransaction =
+                          await platform.invokeMethod('sign', {
+                        'rawTransaction': Uint8List.fromList(transaction),
+                        'n': _base64ToInt(_myWallet.jwk['n']).toString(),
+                        'd': _base64ToInt(_myWallet.jwk['d']).toString(),
+                        'dp': _base64ToInt(_myWallet.jwk['dp']).toString(),
+                        'dq': _base64ToInt(_myWallet.jwk['dq']).toString()
+                      });
+                      final result = await _myWallet.postTransaction(
+                          signedTransaction,
+                          args[0]['0']['last_tx'],
+                          args[0]['0']['reward'],
+                          data: Arweave.decodeBase64EncodedBytes(
+                              args[0]['0']['data']),
+                          tags: Arweave.decodeTags(args[0]['0']['tags']),
+                          targetAddress: args[0]['0']['target'],
+                          quantity: args[0]['0']['quantity']);
+                      debugPrint('Tx status - ${result[0].statusCode.toString()}');
+
+                      return result[1];
                     });
               },
               onConsoleMessage: (InAppWebViewController controller, message) {
@@ -79,11 +137,16 @@ class WebViewContainerState extends State<WebViewContainer> {
                   queries = (Array.from(document.getElementsByTagName('script'))).filter(script => script.text.includes("new FileReader"));
           re = /(?<=function\\s+)(\\w+)(?=\\s*\\(\\w*\\)\\s*\\{[\\s\\S]+new FileReader[\\s\\S]*})/;
           loginFunctionName = (queries[0].text.match(re))[0];
-          console.log(loginFunctionName);
+          console.log("hello");
              window.flutter_inappwebview.callHandler('ableToSign', loginFunctionName, oldSigningFunction).then(function(result) {
                console.log("Hello " + result);
              });
            });
+                ''');
+                webViewController.evaluateJavascript(source: '''
+             window.flutter_inappwebview.callHandler('sign').then(function(result) {
+               console.log("Hello " + result);
+             });
                 ''');
               }))
     ]);
@@ -98,7 +161,7 @@ class Browser extends StatefulWidget {
 
 class BrowserState extends State<Browser> {
   String name;
-
+/*
   void setUrl(String resolvedName) async {
     var trimmedName = resolvedName;
     if (resolvedName.contains('http://')) {
@@ -125,13 +188,18 @@ class BrowserState extends State<Browser> {
             .loadUrl(url: "https://" + trimmedName);
       }
     }
-  }
+  }*/
 
   @override
   Widget build(BuildContext context) {
     final _walletString =
         Provider.of<WalletData>(context, listen: false).walletString;
-    final mes = jsonEncode(_walletString).toString();
+    var key = JsonWebKey.fromJson(jsonDecode(_walletString));
+    final publicKey =
+        Map.from({'kty': key['kty'], 'e': key['e'], 'n': key['n']});
+    final mes = jsonEncode(publicKey);
+    final mes2 = jsonEncode(_walletString.toString());
+
     return Consumer<WalletData>(builder: (context, url, child) {
       return Scaffold(
         appBar: AppBar(
@@ -141,7 +209,8 @@ class BrowserState extends State<Browser> {
                 labelText: 'Address',
               ),
               onSubmitted: (value) {
-                setUrl(value);
+                var url = value.toString();
+                webviewKey.currentState.webViewController.loadUrl(url: url);
               }),
           backgroundColor: Color(0xFFFFFFFF),
         ),
@@ -165,7 +234,9 @@ class BrowserState extends State<Browser> {
                 onPressed: (_walletString != null)
                     ? () => webviewKey.currentState.webViewController
                         .evaluateJavascript(source: '''
-          var walletString = $mes;
+          var wallet = $mes;
+          var walletString = JSON.stringify(wallet);
+          walletString.replace(/"/g,"\\\"")
           queries = (Array.from(document.getElementsByTagName('script'))).filter(script => script.text.includes("new FileReader"));
           re = /(?<=function\\s+)(\\w+)(?=\\s*\\(\\w*\\)\\s*\\{[\\s\\S]+new FileReader[\\s\\S]*})/;
           loginFunctionName = (queries[0].text.match(re))[0];
